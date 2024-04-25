86cefe2ae8df6cec3c3b5ba5f20650f3ca3167ac
Eduardo Rocha Batista <eduardorochabat@gmail.com>
2024-04-25 15:24:05 -0300

    Correções

--- src/main/java/io/github/eduardobatista/rest/resource/PopulateResource.java
+++ src/main/java/io/github/eduardobatista/rest/resource/PopulateResource.java
@@ -2,12 +2,8 @@ package io.github.eduardobatista.rest.resource;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Locale;
-import java.util.Random;
-
-import org.neo4j.ogm.session.Session;
 
 import com.github.javafaker.Faker;
 
@@ -29,57 +25,8 @@ public class PopulateResource {
     TagRepository tagRepository = new TagRepository();
     RecipeRepository recipeRepository = new RecipeRepository();
 
-    // @POST
-    // public Response populate() {
-
-    // Faker faker = new Faker();
-
-    // var dundun = userRepository.save(null, new User("Dundun", "dundun@email.com",
-    // "123456", LocalDate.now(), null,
-    // "Pq eu escolhi java meu deus", "@Dundun", false));
-    // var julinho = userRepository.save(null, new User("Julinho",
-    // "julinho@email.com", "123456", LocalDate.now(),
-    // null, "Jesuscripto", "@Julinho", false));
-    // var yhan = userRepository.save(null, new User("Yhan", "yhan@email.com",
-    // "123456", LocalDate.now(), null,
-    // "ouvido meio cheio", "@Yhan", false));
-    // var richard = userRepository.save(null, new User("Richard",
-    // "richard@email.com", "123456", LocalDate.now(),
-    // null, "MF Destroyer", "@Richard", false));
-
-    // followRepository.save(null, new Follow(dundun, julinho));
-    // followRepository.save(null, new Follow(yhan, richard));
-    // followRepository.save(null, new Follow(julinho, dundun));
-
-    // ArrayList<Tag> listTags = new ArrayList<Tag>();
-
-    // var assados = tagRepository.save(null, new Tag("Assados"));
-    // var cozidos = tagRepository.save(null, new Tag("Cozidos"));
-    // var fritos = tagRepository.save(null, new Tag("Fritos"));
-    // var massas = tagRepository.save(null, new Tag("Massas"));
-    // var carnes = tagRepository.save(null, new Tag("Carnes"));
-    // var jantar = tagRepository.save(null, new Tag("jantares"));
-
-    // listTags.add(assados);
-    // listTags.add(cozidos);
-    // listTags.add(carnes);
-
-    // recipeRepository.save(yhan.getId(), new Recipe("Carne de Panela", "Carne de
-    // gado gostosa", "Carne & Panela", "Butar carne na panela", null, listTags));
-
-    // listTags.clear();
-    // listTags.add(massas);
-    // listTags.
-
-    // recipeRepository.save(dundun.getId(), new Recipe("Lazanha", "Garfield gosta",
-    // "Laz & Zanha", "Faz a lazanha ai mano", null, listTags));
-
-    // return Response.ok().build();
-    // }
-
     @POST
     public Response populateTeste() {
-        Random random = new Random();
         Faker faker = new Faker(new Locale("pt-BR"));
         ArrayList<User> users = new ArrayList<>();
         ArrayList<Tag> tags = new ArrayList<>();
